8e9b521aa2a8fc480190173f05a807abacc30885
tobenik <tobias.nikko@gmail.com>
2024-04-13 18:33:08 +0300

    aqq quey to get all reviews

--- Part 10/reporate/src/graphql/queries.js
+++ Part 10/reporate/src/graphql/queries.js
@@ -23,6 +23,29 @@ export const GET_REPO = gql`
   ${FULL_REPO}
 `;
 
+export const GET_REVIEWS = gql`
+  query getReviews($id: ID!) {
+    repository(id: $id) {
+      id
+      fullName
+      reviews {
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const GET_ME = gql`
   query {
     me {
