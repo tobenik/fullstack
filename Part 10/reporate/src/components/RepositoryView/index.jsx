3cb4fd699d6e268ab2fd77bc193c8fbbf825d33b
tobenik <tobias.nikko@gmail.com>
2024-04-13 18:32:17 +0300

    refactor copmonent

--- Part 10/reporate/src/components/RepositoryView/index.jsx
+++ Part 10/reporate/src/components/RepositoryView/index.jsx
@@ -2,13 +2,41 @@ import { useParams } from "react-router-native";
 import useRepository from "../../hooks/useRepository";
 import RepositoryItem from "../RepositoryList/RepositoryItem";
 import Text from "../Text";
+import useReviews from "../../hooks/useReviews";
+import { FlatList, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+const Header = () => {
+  const { repository } = useRepository(id);
+  return repository ? (
+    <RepositoryItem item={repository} fullView={true} />
+  ) : (
+    <Text>Loading...</Text>
+  );
+};
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryView = () => {
   const params = useParams();
   const id = params.id;
-  const { repository } = useRepository(id);
-  return repository ? (
-    <RepositoryItem item={repository} fullView={true} />
+
+  const { reviews } = useReviews(id);
+
+  const reviewNodes = reviews ? reviews.edges.map((edge) => edge.node) : [];
+
+  return reviewNodes ? (
+    <FlatList
+      ListHeaderComponent={Header}
+      ItemSeparatorComponent={ItemSeparator}
+      data={reviewNodes}
+      renderItem={({ item }) => <Text>{item.text}</Text>}
+    />
   ) : (
     <Text>Loading...</Text>
   );
